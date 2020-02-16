2fe7af0010534b7e9028b402be3effe99ed39b17
Mike Sajak <mike.sajak@gmail.com>
2020-02-16 20:56:31 +0100

    Update building settings panel. Make sure that panel fills horizontally frame, and file editor text field also fills the parent panel.

--- src/main/scala/org/mikesajak/commander/ui/controller/settings/SettingsPanelController.scala
+++ src/main/scala/org/mikesajak/commander/ui/controller/settings/SettingsPanelController.scala
@@ -6,14 +6,12 @@ import javafx.{scene => jfxs}
 import org.mikesajak.commander.ApplicationController
 import org.mikesajak.commander.config.Configuration
 import org.mikesajak.commander.ui.ResourceManager
-import org.mikesajak.commander.ui.controller.settings.SettingsType.{BoolType, ColorType, ExecFileType, IntType}
 import scalafx.Includes._
 import scalafx.collections.ObservableBuffer
 import scalafx.geometry.{Insets, Pos}
 import scalafx.scene.Node
 import scalafx.scene.control.{Button, _}
 import scalafx.scene.layout._
-import scalafx.scene.paint.Color
 import scalafx.scene.text.{Font, FontWeight}
 import scalafx.stage.FileChooser
 import scalafx.util.converter.FormatStringConverter
@@ -27,7 +25,7 @@ case class SettingsGroupPanel(settingsGroup: SettingsGroup, panel: Node)
 
 @sfxml
 class SettingsPanelControllerImpl(categoriesTreeView: TreeView[SettingsGroupPanel],
-                                  categoryPanel: AnchorPane,
+                                  categoryPanel: BorderPane,
                                   okButton: Button,
                                   applyButton: Button,
                                   cancelButton: Button,
@@ -67,8 +65,7 @@ class SettingsPanelControllerImpl(categoriesTreeView: TreeView[SettingsGroupPane
     categoriesTreeView.selectionModel().selectionMode = SelectionMode.Single
 
     categoriesTreeView.selectionModel().selectedItem.onChange{ (_, _, newVal) =>
-      categoryPanel.children.clear()
-      categoryPanel.children = Seq(newVal.value.value.panel)
+      categoryPanel.center = newVal.value.value.panel
     }
   }
 
@@ -86,15 +83,25 @@ class SettingsPanelControllerImpl(categoriesTreeView: TreeView[SettingsGroupPane
       hgap = 5
       vgap = 5
 
+      maxWidth = Double.MaxValue
+      maxHeight = Double.MaxValue
+
+      columnConstraints = Seq(new ColumnConstraints(), new ColumnConstraints() {
+        hgrow = Priority.Always
+        fillWidth = true
+        prefWidth = Region.USE_COMPUTED_SIZE
+        maxWidth = Double.MaxValue
+      })
+
       children = items.zipWithIndex.flatMap { case (item, index) =>
-        val controls = item.itemType match {
-          case BoolType => Seq(mkBoolEditor(item, index))
-          case IntType => Seq(mkLabel(item, index), mkIntEditor(item, index))
-          case ColorType => Seq(mkLabel(item, index), mkColorEditor(item, index))
-          case ExecFileType => Seq(mkLabel(item, index), mkFileEditor(item, index))
-          case _ => Seq(mkLabel(item, index), mkStringEditor(item, index))
+        val controls = item match {
+          case boolItem: BoolSettingsItem => Seq(mkBoolEditor(boolItem))
+          case intItem: IntSettingsItem => Seq(mkLabel(intItem), mkIntEditor(intItem))
+          case colorItem: ColorSettingsItem => Seq(mkLabel(colorItem), mkColorEditor(colorItem))
+          case execFileItem: ExecFileSettingsItem => Seq(mkLabel(execFileItem), mkFileEditor(execFileItem))
+          case _ => Seq(mkLabel(item), mkStringEditor(item))
         }
-        setupControlsProperties(item, controls, index)
+        setupControlsPropertiesAndLayout(item, controls, index)
         controls
       }
     }
@@ -110,7 +117,7 @@ class SettingsPanelControllerImpl(categoriesTreeView: TreeView[SettingsGroupPane
     }
   }
 
-  private def setupControlsProperties(item: SettingsItem, regions: Seq[Region], row: Int): Unit = {
+  private def setupControlsPropertiesAndLayout(item: SettingsItem, regions: Seq[Region], row: Int): Unit = {
     regions.foreach { region =>
       region.alignmentInParent = Pos.BaselineLeft
 
@@ -121,47 +128,52 @@ class SettingsPanelControllerImpl(categoriesTreeView: TreeView[SettingsGroupPane
     else regions.zipWithIndex.foreach { case (region, index) => GridPane.setConstraints(region, index, row) }
   }
 
-  private def mkBoolEditor(item: SettingsItem, row: Int) =
+  private def mkBoolEditor(item: BoolSettingsItem) =
     new CheckBox(item.name) {
-      selected = item.value.asInstanceOf[Boolean]
-      GridPane.setConstraints(this, 0, row, 2, 1)
+      selected = item.value
     }
 
-  private def mkLabel(item: SettingsItem, row: Int) =
+  private def mkLabel(item: SettingsItem) =
     new Label(item.name) {
       alignmentInParent = Pos.BaselineLeft
-      tooltip = new Tooltip(item.description)
-      GridPane.setConstraints(this, 0, row)
     }
 
-  private def mkStringEditor(item: SettingsItem, row: Int) =
+  private def mkStringEditor(item: SettingsItem) =
     new TextField() {
       alignmentInParent = Pos.BaselineLeft
       text = item.value.toString
     }
 
-  private def mkIntEditor(item: SettingsItem, row: Int) =
+  private def mkIntEditor(item: IntSettingsItem) =
     new TextField() {
       val converter = new FormatStringConverter[Number](NumberFormat.getIntegerInstance)
       textFormatter = new TextFormatter(converter)
-      text = item.value.asInstanceOf[Int].toString
+      text = item.value.toString
     }
 
-  private def mkColorEditor(item: SettingsItem, row: Int) =
-    new ColorPicker(item.value.asInstanceOf[Color])
+  private def mkColorEditor(item: ColorSettingsItem) = new ColorPicker(item.value)
 
-  private def mkFileEditor(item: SettingsItem, row: Int) = {
+  private def mkFileEditor(item: ExecFileSettingsItem) = {
     new HBox {
       spacing = 5
+      hgrow = Priority.Always
+      alignmentInParent = Pos.BaselineLeft
+      alignment = Pos.BaselineLeft
+      maxWidth = Double.MaxValue
+      prefWidth = Region.USE_COMPUTED_SIZE
+
       private val textField = new TextField() {
         alignmentInParent = Pos.BaselineLeft
+        alignment = Pos.BaselineLeft
+        hgrow = Priority.Always
+        maxWidth = Double.MaxValue
+        prefWidth = Region.USE_COMPUTED_SIZE
         text = item.value.toString
         tooltip = new Tooltip(item.description)
       }
 
       private val fileChooserButton = new Button {
         text = "..."
-        //graphic =
         alignmentInParent = Pos.BaselineLeft
         tooltip = new Tooltip(item.description)
         onAction = { _ =>
